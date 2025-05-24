e33b7810597f49db1f7df5b7b7f0eac23b3ff99c
Sihui <66382578+BeautySharon@users.noreply.github.com>
2025-05-24 08:09:23 -0700

    Update deploy setting

--- my_website/tailwind.config.js
+++ my_website/tailwind.config.js
@@ -4,10 +4,15 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "sans-serif"],
+        sans: ["Roboto", "sans-serif"], // 全局字体为 Roboto
+      },
+      fontSize: {
+        base: "1rem", // 16px
+        sm: "0.875rem", // 14px
+        xs: "0.75rem", // 12px
       },
       gridTemplateColumns: {
-        "70/30": "70% 28%",
+        "70/30": "70% 28%", // 你自定义的网格比例保留
       },
     },
   },
